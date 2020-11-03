d5702c9a3ea0d1beb13709223389dbcc15b536b8
Spectra <pbldmngz@users.noreply.github.com>
2020-11-03 15:51:01 -0800

    Update skillcraft.R

--- 7mo/SIN/examen_segunda_evaluacion/res/skillcraft.R
+++ 7mo/SIN/examen_segunda_evaluacion/res/skillcraft.R
@@ -1,20 +1,20 @@
+#setwd("C:/")
 lin.reg = read.csv("SkillCraft1_Dataset.csv")
 
 str(lin.reg)
 
-modelo.reg.lin.0 = lm(LeagueIndex ~ APM)
+modelo.reg.lin.0 = lm(lin.reg$LeagueIndex ~ lin.reg$APM)
 
 summary(modelo.reg.lin.0)
 
 predicciones0 = predict(modelo.reg.lin.0)
 
 SSE0 = sum((lin.reg$LeagueIndex - predicciones0) ^ 2)
-SSE0
-
 SST = sum((lin.reg$LeagueIndex - mean(lin.reg$LeagueIndex)) ^ 2)
-SST
-
 r2.0 = 1 - SSE0/SST
+
+SSE0
+SST
 r2.0
 
 plot(lin.reg$APM, lin.reg$LeagueIndex, ylab="League Index", xlab = "Actions per Minute")
